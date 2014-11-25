b19dee2f9fce3fa9731b56b8c6df49d4db701e6f
Mia Filisch <mia.filisch@7digital.com>
2014-11-25 18:42:25 +0000

    Added Nancy module and set fixed port to get OK status code from local host

---
+++ FakeApi/HelloModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace FakeApi
+{
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = parameters => "Hello World";
+        }
+    }
+}
--- Tests/WalkingSkeleton.Tests/WalkingSkeletonTests.cs
+++ Tests/WalkingSkeleton.Tests/WalkingSkeletonTests.cs
@@ -10,12 +10,12 @@ namespace WalkingSkeleton.Tests
         [Test]
         public void It_should_return_a_status_code_of_200()
         {
-            var url = "http://www.google.co.uk";
+            var url = "http://localhost:52343/";
             var request = WebRequest.Create(url);
             var response = request.GetResponse();
 
             Assert.That(((HttpWebResponse)response).StatusCode,Is.EqualTo(HttpStatusCode.OK));
-
+            
         }
     }
 }
