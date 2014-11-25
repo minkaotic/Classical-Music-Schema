b5f818fff8800548c570605a63c7d25f4ad32dad
Mia Filisch <mia.filisch@7digital.com>
2014-11-25 17:55:53 +0000

    Added first useful test getting status code from Google

--- Tests/WalkingSkeleton.Tests/WalkingSkeletonTests.cs
+++ Tests/WalkingSkeleton.Tests/WalkingSkeletonTests.cs
@@ -1,4 +1,5 @@
 ﻿using Classical_Music_Schema;
+using System.Net;
 using NUnit.Framework;
 
 namespace WalkingSkeleton.Tests
@@ -9,7 +10,12 @@ namespace WalkingSkeleton.Tests
         [Test]
         public void It_should_return_a_status_code_of_200()
         {
-            Assert.That(Api.StatusCode(), Is.EqualTo("200"));
+            var url = "http://www.google.co.uk";
+            var request = WebRequest.Create(url);
+            var response = request.GetResponse();
+
+            Assert.That(((HttpWebResponse)response).StatusCode,Is.EqualTo(HttpStatusCode.OK));
+
         }
     }
 }
