019c3499a706eedaa84fd972cf79dd15234d15f3
Mia Filisch <mia.filisch@7digital.com>
2014-11-25 12:48:16 +0000

    trying to fix failing build

---
+++ WalkingSkeleton.Tests/WalkingSkeletonTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace WalkingSkeleton.Tests
+{
+    [TestFixture]
+    public class WalkingSkeletonTests
+    {
+        [Test]
+        public void It_should_return_a_status_code_of_200()
+        {
+            Assert.That(Api.Status(), Is.EqualTo("200"));
+        }
+    }
+
+    public class Api
+    {
+        public static string Status()
+        {
+            return "200";
+        }
+    }
+}
