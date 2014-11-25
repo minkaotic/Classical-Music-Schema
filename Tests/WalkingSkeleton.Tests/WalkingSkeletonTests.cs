c123ca34163a7b2d7b7de6e1e2fa81981b645beb
Mia Filisch <mia.filisch@7digital.com>
2014-11-25 17:30:15 +0000

    ..

--- Tests/WalkingSkeleton.Tests/WalkingSkeletonTests.cs
+++ Tests/WalkingSkeleton.Tests/WalkingSkeletonTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Classical_Music_Schema;
+﻿using Classical_Music_Schema;
 using NUnit.Framework;
 
 namespace WalkingSkeleton.Tests
