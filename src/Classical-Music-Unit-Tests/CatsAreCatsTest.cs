b228fe3337e32fe33d6f84f4aef01271cb6d4b13
Mia Filisch <mia.filisch@7digital.com>
2014-11-26 16:25:39 +0000

    Added placeholder unit[A test to guide Teamcity setup

---
+++ src/Classical-Music-Unit-Tests/CatsAreCatsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Classical_Music_Unit_Tests
+{
+    [TestFixture]
+    public class CatsAreCatsTest
+    {
+        [Test]
+        public void A_cat_is_a_cat()
+        {
+            string cat1 = "cat";
+            string cat2 = "cat";
+
+            Assert.That(cat1, Is.EqualTo(cat2));
+        }
+    }
+}
