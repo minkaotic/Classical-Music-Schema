44baffac10b2356d7dcd8e8fa3679d2e9ca21d99
Mia Filisch <mia.filisch@7digital.com>
2014-12-19 11:48:33 +0000

    Naming convention in Nancy modules is now more consistent and scaleable

---
+++ src/Classical-Music-Nancy/ReleasePageModule.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+
+namespace Classical_Music_Nancy
+{
+    public class ReleasePageModule : NancyModule
+    {
+
+        public ReleasePageModule()
+        {
+            Get["/release/1"] = parameters => RenderReleasePage();
+        }
+
+        private static string RenderReleasePage()
+        {
+            return "{\n\"release_title\": \"Mahler: Symphony No. 1\"\n}";
+        }
+    }
+}
---
+++ src/Classical-Music-Nancy/StatusPageModule.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using Nancy;
+
+namespace Classical_Music_Nancy
+{
+    public class StatusPageModule : NancyModule
+    {        
+
+        public StatusPageModule()
+        {
+            Get["/status"] = parameters => RenderStatusPage();            
+        }
+
+        private static string RenderStatusPage()
+        {
+            var newline = "<p/>";
+            var databaseConnection = new DatabaseConnection("http://10.120.17.75:7474/db/data");
+            if (databaseConnection.ConnectToDB())
+                return DbOnline() + newline + OutputVersionNumber();
+            else
+                return DbNotFound();
+        }
+
+        private static string OutputVersionNumber()
+        {
+            return "Version: " + Assembly.GetExecutingAssembly().GetName().Version;
+        }
+
+        private static string DbNotFound()
+        {
+            return "Database: NOT FOUND";
+        }
+
+        private static string DbOnline()
+        {
+            return "Database: ONLINE";
+        }
+    }
+}
