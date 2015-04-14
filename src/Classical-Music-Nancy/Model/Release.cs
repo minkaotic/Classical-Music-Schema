0be8eff9170faf35ac856f46bcff8fa807f88aba
Mia Filisch <mia.filisch@7digital.com>
2015-04-14 17:26:06 +0100

    ~/release/{id} endpoint now also returns UPC and release date.

--- src/Classical-Music-Nancy/Model/Release.cs
+++ src/Classical-Music-Nancy/Model/Release.cs
@@ -7,10 +7,13 @@ namespace Classical_Music_Nancy.Model
 		[JsonProperty(PropertyName = "name")]
 		public string Title { get; set; }
 
+		[JsonProperty(PropertyName = "release_date")]
+		public string ReleaseDate { get; set; }
+
+		[JsonProperty(PropertyName = "upc")]
+		public string Upc { get; set; }
+
 		[JsonProperty(PropertyName = "sd_id")]
 		public int SevenDigitalId { get; set; }
-
-		//[JsonProperty(PropertyName = "upc")]
-		//public int Upc { get; set; }
 	}
 }
